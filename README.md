e4a68d5bd83c4153f9f4ef1838efcc06103c3450
Guilherme Souza <dev.guih@gmail.com>
2025-07-26 13:38:11 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+<table width="100%">
+  <tr>
+  <td>
+<p align="center">
+  <samp>
+    Software engineer based in Brazil.<br/ > <a href="mailto:g@gmds.dev">Get in touch</a><br/>
+    <a href="https://gmds.dev">Website</a> .
+    <a href="https://x.com/gmdsdev">Twitter</a> .
+    <a href="https://linkedin.com/in/gmdsdev">LinkedIn</a>
+  </samp>
+</p>
+</td>
+</tr>
+</table>
---
+++ index.js
@@ -0,0 +1,17 @@
+const backgroundColors = [
+  "#CDBAF3",
+  "#F8D7FC",
+  "#FCEEFE",
+  "#F9FBD2",
+  "#F9F9EA",
+  "#9FD1F7",
+  "#EDEDEF",
+  "#FFFFFF",
+];
+
+window.addEventListener("load", () => {
+  const body = document.body;
+  const backgroundColor =
+    backgroundColors[Math.floor(Math.random() * backgroundColors.length)];
+  body.style.backgroundColor = backgroundColor;
+});
