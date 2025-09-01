5d33914016fdedc18c1c28af0dba4f4b5feda390
Guilherme Souza <dev.guih@gmail.com>
2025-08-31 22:04:24 -0300

    feat: remove random colors and improved page performance

--- src/main.js
+++ src/main.js
@@ -2,23 +2,5 @@ import "./style.css";
 import { injectSpeedInsights } from "@vercel/speed-insights";
 import { inject as injectAnalytics } from "@vercel/analytics";
 
-const backgroundColors = [
-  "#CDBAF3",
-  "#F8D7FC",
-  "#FCEEFE",
-  "#F9FBD2",
-  "#F9F9EA",
-  "#9FD1F7",
-  "#EDEDEF",
-  "#FFFFFF",
-];
-
-window.addEventListener("load", () => {
-  const body = document.body;
-  const backgroundColor =
-    backgroundColors[Math.floor(Math.random() * backgroundColors.length)];
-  body.style.backgroundColor = backgroundColor;
-});
-
 injectSpeedInsights();
 injectAnalytics();
